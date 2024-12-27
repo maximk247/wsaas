b4dc6b3c541146fa5c6e9f7384f8f479d84b2189
maximk247 <kiyaniytsamaxim@gmail.com>
2024-12-27 15:34:47 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// Примитивный тип данных
+let stroka = 'fdpfdpf'
+let number = 2134
+let boolean = true // false
+let null1 = null // пустота, ничего (сознательное действие) -> строка, число
+let undef = undefined // пустота, ничего, но это скорее не найдено (поведение, которое говорит нам об ошибке скорее всего)
+let bigInt = 100n;
+let symbol = Symbol('dsds');
+
+// Ссылочный тип данных
+let object = {
+    name: 'Dima',
+    age: 25,
+    isMarried: true
+} // довольно часто используемый тип данных
+
